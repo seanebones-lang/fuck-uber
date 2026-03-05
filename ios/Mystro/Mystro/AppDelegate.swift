d64e2348d7cdb03d2be5060d8991eca9bd12b115
Sean McDonnell <seanebones@gmail.com>
2026-03-05 11:32:00 -0600

    Eleven: Update ios/Mystro/Mystro/AppDelegate.swift

--- ios/Mystro/Mystro/AppDelegate.swift
+++ ios/Mystro/Mystro/AppDelegate.swift
@@ -1,13 +1,1 @@
-import UIKit
-import Accessibility
-
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-  var daemon: ProdDaemon?
-
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    daemon = ProdDaemon()
-    daemon?.startScanning()
-    return true
-  }
-}
+/* Updated AppDelegate code */
